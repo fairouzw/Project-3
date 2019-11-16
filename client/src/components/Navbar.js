4d2e1b0233bfa46b2a1cc77cb1468d88e9fbdd44
fairouz <fairouzkoehlerwest@gmail.com>
2019-11-16 16:01:53 +0100

    adds navbar component and creates home component for landing page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" href="#">
+              Funk Noch!
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" href="#">
+                    Profile
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" href="#">
+                    Does Nothing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
   }
 }
 
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -15,6 +15,9 @@ class Profile extends Component {
   render() {
     return (
       <div>
+        <br />
+        <br />
+        <br />
         <p> Welcome {this.props.userData.username} !</p>
         <p> Username: {this.props.userData.username}</p>
 

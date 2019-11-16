d089bb0317d3cb08e7620878e200aee120a12887
Lena Lau <frau.lena.lau@gmail.com>
2019-11-16 16:57:56 +0100

    creates post-model and route ---> post works now

---
+++ models/post-model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//const moment = require('moment')
+
+const postSchema = new Schema(
+    {
+        imgUrl: String,
+        location: String,
+        description: String,
+        postname: String,
+        owner: { type: Schema.Types.ObjectId, ref: 'User' },
+        categories: {
+            type: String, // try to implement following category-tags
+            enum: [
+                "food",
+                "electronics",
+                "furniture",
+                "clothes",
+                "dishes"
+            ]
+        },
+        //comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+        likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        expireDate: {
+            type: String,
+            //default: () => moment().format('MMM Do YY')
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;

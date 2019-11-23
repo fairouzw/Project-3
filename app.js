476bc9b9286c78c3c29ff927370df4aa8a01cbfb
fairouz <fairouzkoehlerwest@gmail.com>
2019-11-23 11:55:03 +0100

    deployment config

--- app.js
+++ app.js
@@ -47,15 +47,11 @@ app.use(
   })
 );
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.use("/", indexRouter);
 // app.use("/users", usersRouter);
@@ -77,15 +73,15 @@ app.use(passport.session());
 app.use("/api", authRoutes);
 app.use("/api", profRoutes);
 app.use("/api/posts", postRoutes);
-app.use('/api', require('./routes/file-upload-routes'));
+app.use("/api", require("./routes/file-upload-routes"));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -93,6 +89,11 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
+});
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
 });
 
 module.exports = app;
--- configs/cloudinary-setup.js
+++ configs/cloudinary-setup.js
@@ -1,19 +1,19 @@
-const cloudinary = require('cloudinary');
-const cloudinaryStorage = require('multer-storage-cloudinary');
-const multer = require('multer');
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.cloudName,
-  api_key: process.env.cloudKey,
-  api_secret: process.env.cloudSecret
+  cloud_name: process.env.CLOUDNAME,
+  api_key: process.env.CLOUDKEY,
+  api_secret: process.env.CLOUDSECRET
 });
 
 var storage = cloudinaryStorage({
   cloudinary,
-  folder: 'thing-gallery', // The name of the folder in cloudinary
-  allowedFormats: ['jpg', 'png'],
+  folder: "thing-gallery", // The name of the folder in cloudinary
+  allowedFormats: ["jpg", "png"],
   // params: { resource_type: 'raw' }, => this is in case you want to upload other type of files, not just images
-  filename: function (req, res, cb) {
+  filename: function(req, res, cb) {
     cb(null, res.originalname); // The file on cloudinary would have the same name as the original file name
   }
 });

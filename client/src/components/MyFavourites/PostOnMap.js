afd18b53ab1bc2d58bd32acd62db5d977afe4098
fairouz <fairouzkoehlerwest@gmail.com>
2019-12-09 19:34:55 +0100

    updates favMap

--- client/src/components/MyFavourites/PostOnMap.js
+++ client/src/components/MyFavourites/PostOnMap.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "../../App.css";
-import DisplayPost from '../Posts/DisplayPost'
+import DisplayPost from "../Posts/DisplayPost";
 import Header from "../Home/Header";
-import FavMap from './FavMap';
-import axios from 'axios'
+import FavMap from "./FavMap";
+import axios from "axios";
 import { withRouter } from "react-router-dom";
 
-import {
-  Card,
-  CardBody,
-  Container,
-  Row,
-  Col,
-  
-} from "reactstrap";
+import { Card, CardBody, Container, Row, Col } from "reactstrap";
 
- class PostDetails extends Component {
+class PostDetails extends Component {
   constructor() {
     super();
     this.state = {
@@ -46,30 +39,36 @@ import {
   render() {
     return (
       <div className="main-content" ref="mainContent">
-    <Header />
- <Container className="mt--7" fluid>
-
- <Row>
-   <Col className="mb-5 mb-xl-0" xl="8">
-     <Card className="bg-gradient-secondary shadow">
-         {/* MAP */}
-           {this.state.selectedPost ? <FavMap selectedPost={this.state.selectedPost} posts={this.state.filteredListOfPosts} /> : <p>Loading...</p> }
-     </Card>
-   </Col>
-   <Col xl="4">
-     <Card className="bg-gradient-secondary shadow">
-       <CardBody>
-         {/* DISPLAYED POST */}
-  {this.state.selectedPost ?  <DisplayPost selectedPost={this.state.selectedPost} /> : 
-  <p> Select something on the map/Display an image/show something</p>
-  }
-       </CardBody>
-     </Card>
-   </Col>
- </Row>
- </Container>
-  </div>
-    )
+        <Header />
+        <Container className="mt--7" fluid>
+          <Row>
+            <Col className="mb-5 mb-xl-0" xl="8">
+              <Card className="bg-gradient-secondary shadow">
+                {/* MAP */}
+                {this.state.selectedPost ? (
+                  <FavMap
+                    selectedPost={this.state.selectedPost}
+                    posts={this.state.filteredListOfPosts}
+                  />
+                ) : (
+                  <p>Loading...</p>
+                )}
+              </Card>
+            </Col>
+            <Col xl="4">
+              <Card className="bg-gradient-secondary shadow">
+                <CardBody>
+                  {/* DISPLAYED POST */}
+                  {this.state.selectedPost ? (
+                    <DisplayPost selectedPost={this.state.selectedPost} />
+                  ) : null}
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 

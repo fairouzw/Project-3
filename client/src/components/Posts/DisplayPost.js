d82d5c98f18717f3b495a52b0742899a0406219a
Lena Lau <frau.lena.lau@gmail.com>
2019-12-13 16:21:52 +0100

    fixes-message-bug

--- client/src/components/Posts/DisplayPost.js
+++ client/src/components/Posts/DisplayPost.js
@@ -14,7 +14,7 @@ class DisplayPost extends Component {
           <div className="posted-by-container">
             <div className="posted-by-box">
               {/* posted by */}  <i id="btn-fg" className="ni ni-single-02" />
-              <Link to={`/messages/new-message/${this.props.selectedPost.owner}`}>
+              <Link to={`/messages/new-message/${this.props.selectedPost.owner._id}`}>
                 {" "}
                 <span className="owner icon-text">
                   {this.props.selectedPost.owner.username}{" "}

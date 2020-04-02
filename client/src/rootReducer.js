30e21158b3453a5ad06abc80343f177bf8756c30
fairouz <fairouzkoehlerwest@gmail.com>
2020-04-02 18:42:36 +0200

    adds email field and deletes redux dir

--- client/src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-// import {starWarsReducer} from './components/starWars/starWarsReducer';
-// import {weatherReducer} from './components/weather/weatherReducer';
-
-export default combineReducers({
-starWarsReducer: starWarsReducer,
-weatherReducer: weatherReducer
-});
